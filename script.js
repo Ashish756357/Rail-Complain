e5df0aeed7e473b8873b1d7a0306dfbc6e4339b6
Ashish Raj <126260804+Ashish756357@users.noreply.github.com>
2024-09-02 09:35:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+$('#form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+    var $this = $(this),
+        label = $this.prev('label');
+  
+        if (e.type === 'keyup') {
+              if ($this.val() === '') {
+            label.removeClass('active highlight');
+          } else {
+            label.addClass('active highlight');
+          }
+      } else if (e.type === 'blur') {
+          if( $this.val() === '' ) {
+              label.removeClass('active highlight'); 
+              } else {
+              label.removeClass('highlight');   
+              }   
+      } else if (e.type === 'focus') {
+        
+        if( $this.val() === '' ) {
+              label.removeClass('highlight'); 
+              } 
+        else if( $this.val() !== '' ) {
+              label.addClass('highlight');
+              }
+      }
+  
+  });
+  
+  $('.tab a').on('click', function (e) {
+    
+    e.preventDefault();
+    
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().removeClass('active');
+    
+    target = $(this).attr('href');
+  
+    $('.tab-content > div').not(target).hide();
+    
+    $(target).fadeIn(800);
+    
+  });
+
+// Login Action
+  <script>
+    document.getElementById('loginForm').addEventListener('submit', function(event) {
+        // You can perform validation or AJAX requests here before redirecting
+        // For this example, we'll assume the login is successful and redirect immediately
+        
+        // Redirect to Complain.html
+        window.location.href = 'Complain.html'
+    });
+</script>
